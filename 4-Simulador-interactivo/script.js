4520688238d607dc88a21972cec74a2134324f3a
garcia-leandro-lucas <garcia.leandrolucas@gmail.com>
2022-05-21 18:52:32 -0300

    Se agrega funciones en el ejercicio de simulador interactivo

--- 4-Simulador-interactivo/script.js
+++ 4-Simulador-interactivo/script.js
@@ -8,20 +8,31 @@ const COSTO_DE_ENVIO_CABA = 600;
 const COSTO_DE_ENVIO_GBA = 900;
 const COSTO_DE_ENVIO_INT_GBA = 600;
 
-let seleccion_de_producto = prompt(`Por favor selecciones un producto del carrito de compra:\n A: Zapatillas Nike: $ ${ZAPATILLAS_NIKE} \nB: Zapatillasadiddas: $ ${ZAPATILLAS_ADIDAS}`);
-	
-let zona_de_envio = prompt("Por favor, seleccione su zona de envío: A: CABA \nB: Gran Buenos Aires\n C: Interior de Buenos Aires\n")
+/* Calcula el precio total al usuario cuando realiza la selección del producto y el envío. */
+function costo_total( precio_de_producto , costo_de_envio) {
+	return precio_de_producto + costo_de_envio;
+}
 
-let costo_total = 0;
+/* Acá le informamos su elección y el precio final */
+function mensaje_de_finalizacion(seleccion_de_producto, zona_de_envio, precio_total ){
+	alert(`Ustedes seleccionó la opción ${seleccion_de_producto} y su zona de envío es: ${zona_de_envio}.`);
+	alert("Su total a abonar es:" + precio_total);
+}
+
+let seleccion_de_producto = prompt(`Por favor selecciones un producto del carrito de compra:\nA: Zapatillas Nike: $ ${ZAPATILLAS_NIKE}\nB: Zapatillasadiddas: $ ${ZAPATILLAS_ADIDAS}`);
+	
+let zona_de_envio = prompt("Por favor, seleccione su zona de envío:\nA: CABA \nB: Gran Buenos Aires\nC: Interior de Buenos Aires\n")
+
+let precio_total = 0;
 
 	switch (zona_de_envio) {
 		case 'A':
 			alert(`El costo de envío de CABA es de: $ ${COSTO_DE_ENVIO_CABA}`);
-	
+
 			if(seleccion_de_producto == "A") {
-				costo_total = ZAPATILLAS_NIKE + COSTO_DE_ENVIO_CABA;
+				precio_total = costo_total( ZAPATILLAS_NIKE , COSTO_DE_ENVIO_CABA);
 			}else {
-				costo_total = ZAPATILLAS_ADIDAS + COSTO_DE_ENVIO_CABA;
+				precio_total = costo_total( ZAPATILLAS_ADIDAS , COSTO_DE_ENVIO_CABA);
 			}
 		break;
 	
@@ -29,9 +40,9 @@ let costo_total = 0;
 			alert(`El costo de envío al Gran Buenos Aires es de: $ ${COSTO_DE_ENVIO_GBA}`);
 	
 			if(seleccion_de_producto == "A") {
-				costo_total = ZAPATILLAS_NIKE + COSTO_DE_ENVIO_GBA;
+				precio_total = costo_total( ZAPATILLAS_NIKE , COSTO_DE_ENVIO_GBA);
 			}else {
-				costo_total = ZAPATILLAS_ADIDAS + COSTO_DE_ENVIO_GBA;
+				precio_total = costo_total( ZAPATILLAS_ADIDAS , COSTO_DE_ENVIO_GBA);
 			}
 		break;
 	
@@ -39,9 +50,9 @@ let costo_total = 0;
 			alert(`El costo de envío al interior de Buenos Aires es de: $ ${COSTO_DE_ENVIO_INT_GBA}`);
 	
 			if(seleccion_de_producto == "A") {
-				costo_total = ZAPATILLAS_NIKE + COSTO_DE_ENVIO_INT_GBA;
+				precio_total = costo_total( ZAPATILLAS_NIKE , COSTO_DE_ENVIO_INT_GBA);
 			}else {
-				costo_total = ZAPATILLAS_ADIDAS + COSTO_DE_ENVIO_INT_GBA;
+				precio_total = costo_total( ZAPATILLAS_ADIDAS , COSTO_DE_ENVIO_INT_GBA);
 			}
 		break;
 	
@@ -49,12 +60,9 @@ let costo_total = 0;
 			alert('Lo lamentamos, por el momento no disponemos de esa zona de envío. Por favor, seleccione su zona de envío: A: CABA \nB: Gran Buenos Aires\n C: Interior de Buenos Aires\n');
 	}
 
+mensaje_de_finalizacion(seleccion_de_producto, zona_de_envio, precio_total );
 
-let mensaje_de_pedido = alert(`Ustedes seleccionó la opción ${seleccion_de_producto} y su zona de envío es: ${zona_de_envio}.`);
-let mensaje_de_costo_total = alert("Su total a abonar es:" + costo_total);
 
-alert(mensaje_de_pedido);
-alert(mensaje_de_costo_total);
 
 
 
